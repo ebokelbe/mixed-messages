b8f3767dfc1f8b0829c8c272f5a105158211a621
ebokelbe <ethan.bokelberg@gmail.com>
2021-09-13 18:17:06 -0400

    added arrays of message parts

---
+++ messages.js
@@ -0,0 +1,16 @@
+// Every time a user runs a program, they should get a new, randomized output.
+// To make your program truly random, the message that it outputs should be made up of at least three different pieces of data.
+
+const messageGenerator = () => {
+    // message should be composed of 3 different parts, randomly pick each one
+
+    const part1 = ['Once upon a time ', 'In a far off land ', 'In a hole in the ground '];
+
+    const part2 = ['there was a heroic king ', 'there was a famous explorer ', 'there lived a hobbit, '];
+
+    const part3 = ['named Arthur, king of the Britons.', 'named Marco Polo.', 'named Bilbo Baggins.'];
+
+    
+
+}
+
